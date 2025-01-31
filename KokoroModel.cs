57325e3ea67a01f317cf9b1c678bb5e382224ce6
Lyrcaxis <lyrcaxis@gmail.com>
2025-01-31 04:36:25 +0200

    Single-batch inference working fully (CPU only)

---
+++ KokoroModel.cs
@@ -0,0 +1,49 @@
+﻿namespace KokoroSharp;
+
+using Microsoft.ML.OnnxRuntime;
+using Microsoft.ML.OnnxRuntime.Tensors;
+
+using System.Collections.Generic;
+
+public class KokoroModel {
+    readonly InferenceSession _session;
+
+    static float[,,] voiceStyle;
+
+    public KokoroModel(string modelPath, string voicePath = null) {
+        var options = new SessionOptions() { EnableMemoryPattern = true, InterOpNumThreads = 8, IntraOpNumThreads = 8 };
+        //options.AppendExecutionProvider_CUDA(0);
+        _session = new InferenceSession(modelPath, options);
+    }
+
+    public float[] TokensToWav_SingleBatch(int[] tokens, string voicePath, float speed = 1) {
+        var voiceStyle = NumSharp.np.Load<float[,,]>(voicePath); // 510, 1, 256
+        int maxPhonemeLength = voiceStyle.GetLength(0);
+        var voiceDims = voiceStyle.GetLength(2);
+
+        if (tokens.Length > maxPhonemeLength) { Array.Resize(ref tokens, maxPhonemeLength); }
+
+        // Form Kokoro's input (<start>{text}<end>)
+        var inputTokens = new int[tokens.Length + 2];
+        inputTokens[0] = 0; // <start>
+        Array.Copy(tokens, 0, inputTokens, 1, tokens.Length);
+        inputTokens[^1] = 0; // <end>
+
+        var tokenTensor = new DenseTensor<long>([1, inputTokens.Length]); // Batch size = 1
+        for (int i = 0; i < inputTokens.Length; i++) { tokenTensor[0, i] = inputTokens[i]; }
+        var styleTensor = new DenseTensor<float>([1, voiceDims]);
+        for (int j = 0; j < voiceDims; j++) { styleTensor[0, j] = voiceStyle[tokens.Length, 0, j]; }
+        var speedTensor = new DenseTensor<float>(new[] { speed }, [1]);
+        
+        // Create input data map
+        var inputs = new List<NamedOnnxValue> {
+            NamedOnnxValue.CreateFromTensor("tokens", tokenTensor),
+            NamedOnnxValue.CreateFromTensor("style", styleTensor),
+            NamedOnnxValue.CreateFromTensor("speed", speedTensor)
+        };
+        
+        using var results = _session.Run(inputs);
+        var x = results[0].AsTensor<float>();
+        return [.. x];
+    }
+}
--- Program.cs
+++ Program.cs
@@ -4,17 +4,16 @@ using NAudio.Wave;
 
 internal class Program {
     static void Main(string[] args) {
-        var mPath = @"C:\Users\lyrco\Downloads\kokoro-v0_19.onnx";
+        var kokoro = new KokoroModel(@"C:\Users\lyrco\Downloads\kokoro-v0_19.onnx");
+        
         var vPath = @"C:\Users\lyrco\Downloads\espeak-ng\voiceStyle.npy";
-        var kokoro = new KokoroMinimal(mPath, vPath);
         while (true) {
             var x = Console.ReadLine();
             var s = Tokenizer.Tokenize(x);
             Debug.WriteLine(string.Join(',', s));
 
-            var f = kokoro.TokensToWav_SingleBatch(s, 1);
-            Debug.WriteLine(f.Length);
-            PlayAudio(f);
+            var wave = kokoro.TokensToWav_SingleBatch(s, vPath, 1);
+            PlayAudio(wave);
         }
     }
 
