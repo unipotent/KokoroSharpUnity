10171da730073549e0a55475f0a3e94271d8f8ff
Martin Evans <martindevans@gmail.com>
2025-02-13 21:06:38 +0000

    Blocking KokoroEngine (#7)
    * Modified KokoroEngine to block, instead of poll with a very short timer. This should be more responsive (no longer wasting 10ms between every job) and less wasteful (no longer spinning when waiting for work).
    
    * - Fixed formatting/naming
     - Added back in an inner loop for multi step jobs
     - Catching cancellation exception within worker thread
    
    * Removed `private` modifier
    
    * Disposing and cancelling within the worker thread
    
    * Apply suggestions from code review
    
    trim cancellation token and try-catch (+ minor styling)
    
    ---------
    
    Co-authored-by: Lyrcaxis <32474602+Lyrcaxis@users.noreply.github.com>

--- Core/KokoroEngine.cs
+++ Core/KokoroEngine.cs
@@ -8,24 +8,16 @@ using System.Collections.Concurrent;
 /// <remarks> Contains a background worker thread that dispatches queued jobs/actions linearly. </remarks>
 public class KokoroEngine : IDisposable {
     protected readonly KokoroModel model;
-    protected readonly ConcurrentQueue<KokoroJob> queuedJobs = [];
-
-    volatile bool hasExited;
+    protected readonly BlockingCollection<KokoroJob> queuedJobs = [];
 
     /// <summary> Creates a new Kokoro Engine instance, loading the model into memory and initializing a background worker thread to continuously scan for newly queued jobs, dispatching them in order, when it's free. </summary>
     /// <remarks> If 'options' is specified, the model will be loaded with them. This is particularly useful when needing to run on non-CPU backends, as the default backend otherwise is the CPU with 8 threads. </remarks>
     public KokoroEngine(string modelPath, SessionOptions options = null) {
         model = new KokoroModel(modelPath, options);
 
-        new Thread(async () => {
-            while (!hasExited) {
-                await Task.Delay(10);
-                while (!hasExited && queuedJobs.TryDequeue(out var job)) {
-                    while (!hasExited && !job.isDone) {
-                        job.Progress(model);
-                        await Task.Delay(1);
-                    }
-                }
+        new Thread(() => {
+            foreach (var kokoroJob in queuedJobs.GetConsumingEnumerable()) {
+                while (!kokoroJob.isDone) { kokoroJob.Progress(model); }
             }
         }).Start();
     }
@@ -33,18 +25,15 @@ public class KokoroEngine : IDisposable {
     /// <summary> Enqueues a job for the Kokoro TTS engine, scheduling it to be processed when the engine is free. </summary>
     /// <remarks> The job will be automatically dispatched when all prior jobs have been completed or canceled. Canceled jobs resolve and get skipped when their order arrives. </remarks>
     public KokoroJob EnqueueJob(KokoroJob job) {
-        ObjectDisposedException.ThrowIf(hasExited, this);
-        queuedJobs.Enqueue(job);
+        queuedJobs.Add(job);
         return job;
     }
 
     /// <summary> Immediately cancels any ongoing registered jobs and playbacks, frees memory taken by the model, and notifies the background worker thread to exit. </summary>
     /// <remarks> Note that this will not free up memory that the voices take (~25MB if ALL languages are loaded). </remarks>
     public virtual void Dispose() {
-        hasExited = true;
         foreach (var job in queuedJobs) { job.Cancel(); }
-        queuedJobs.Clear();
+        queuedJobs.CompleteAdding();
         model.Dispose();
-        GC.SuppressFinalize(this);
     }
 }
